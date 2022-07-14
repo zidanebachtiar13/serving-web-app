90fd26c7ce156e8d08f6f823f38acfee5416bd6a
zidanebachtiar13 <zidanebachtiar13@gmail.com>
2022-07-14 13:51:50 +0700

    update soal salah

--- src/serving_web_app/handler.clj
+++ src/serving_web_app/handler.clj
@@ -17,7 +17,6 @@
   (POST "/quiz" [problems topic no0-id no0 no1-id no1 no2-id no2 no3-id no3 no4-id no4 no5-id no5 no6-id no6 no7-id no7] (do (problem/reset-score)
                                                                                                                              (problem/reset-soal-salah)
                                                                                                                              (problem/reset-tak-terjawab)
-                                                                                                                             (problem/reset-jawaban)
                                                                                                                              (problem/change-subject topic)
                                                                                                                              (problem/save-soal problems)
                                                                                                                              (problem/save-jawaban no0 no1 no2 no3 no4 no5 no6 no7)
